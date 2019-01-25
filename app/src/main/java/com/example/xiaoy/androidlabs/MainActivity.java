11fb29aa366d70ca4338181d1f5b8fe0c8dcca65
xiaoyuding <ding0054@algonquinlive.com>
2019-01-25 13:42:47 -0500

    lab2 second genggai

--- app/src/main/java/com/example/xiaoy/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/xiaoy/androidlabs/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.xiaoy.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setContentView(R.layout.activity_main_grid);
+        //setContentView(R.layout.activity_main_linear);
+        //setContentView(R.layout.activity_main_relative);
+
+        Button startButton = (Button) findViewById(R.id.button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        //Log.e(ACTIVITY_NAME, "In onCreate()");
     }
-}
+    }
+
