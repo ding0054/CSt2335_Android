30a21659249f2192d868e07f93ed44060d382893
xiaoyuding <ding0054@algonquinlive.com>
2019-02-05 16:13:18 -0500

    Lab 03 Final v

--- app/src/main/java/com/example/xiaoy/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/xiaoy/androidlabs/ProfileActivity.java
@@ -1,8 +1,16 @@
 package com.example.xiaoy.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
+import android.graphics.Bitmap;
+import android.widget.EditText;
+import android.provider.MediaStore;
 
 import static com.example.xiaoy.androidlabs.R.id.clickButton_profile;
 
@@ -10,7 +18,7 @@ public class ProfileActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "ProfileActivity";
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    ImageButton imageButton;
+     ImageButton iButton;
 
 
     @Override
@@ -18,6 +26,31 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        imageButton =(ImageButton)findViewById(clickButton_profile);
+        iButton = (ImageButton) findViewById(R.id.clickButton_profile);
+        iButton.setOnClickListener(new View.OnClickListener() {
+            final int REQUEST_IMAGE_CAPTURE = 1;
+            @Override
+            public void onClick(View view) {
+            }
+        private void dispatchTakePictureIntent(){
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+            }
+        }
+        });
+
+        Log.e(ACTIVITY_NAME,"IN onCREATE()");
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap)extras.get("data");
+            iButton.setImageBitmap(bitmap);
+        }
+        Log.e(ACTIVITY_NAME,"IN onActivityRESULT");
     }
 }
