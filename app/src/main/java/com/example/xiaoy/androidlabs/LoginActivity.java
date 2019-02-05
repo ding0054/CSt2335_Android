3674c7b45d6b113bb5e437c2ba306ba34eff835e
xiaoyuding <ding0054@algonquinlive.com>
2019-02-05 13:43:26 -0500

    Lab 03 edting 2 java

--- app/src/main/java/com/example/xiaoy/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/xiaoy/androidlabs/LoginActivity.java
@@ -1,15 +1,18 @@
 package com.example.xiaoy.androidlabs;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME="LoginActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +28,52 @@ public class LoginActivity extends AppCompatActivity {
 
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 EditText loginName= (EditText)findViewById(R.id.editText_email_login);
+                editor.putString("DefaultEmail",loginName.getText().toString());
+                editor.commit();
+                Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                startActivity(intent);
             }
         });
-            //SharedPreferences sharedPref = getSharedPreferences(
-                //"LoginFile", Context.MODE_PRIVATE);
-        //int numTimeRun = sharedPref.getInt("TIMES_RUN", 0);
-        //EditText loginName = (EditText) findViewById(R.id.editTextLogin);
-        //loginName.setText( sharedPref.getString("DefaultEmail",""));
-
-        //Log.i(ACTIVITY_NAME, "In onCreate()");
+        Log.e(ACTIVITY_NAME,"IN onCREATE()");
     }
+
+
+
+    public void onStart(){
+        super.onStart();
+        Log.e(ACTIVITY_NAME,"IN onSTART()");
+    }
+
+
+  public void onResume(){
+        super.onResume();
+        SharedPreferences sharedPreferences = getSharedPreferences(
+                "LoginFile",Context.MODE_PRIVATE);
+        int timeRun = sharedPreferences.getInt("TIME_RUN",0);
+        EditText loginName = (EditText) findViewById(R.id.editText_email_login);
+        loginName.setText(sharedPreferences.getString("DefaultEmail",""));
+        Log.e(ACTIVITY_NAME,"IN onRESUME()");
+  }
+
+
+  public void onPause(){
+        super.onPause();
+        SharedPreferences sharedPreferences = getSharedPreferences(
+                "LoginFile",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        EditText loginName = (EditText) findViewById(R.id.editText_email_login);
+        editor.putString("DefaultEmail",loginName.getText().toString());
+        editor.commit();
+        Log.e(ACTIVITY_NAME,"IN onPAUSE()");
+  }
+
+  protected  void  onStop(){
+        super.onStop();
+        Log.e(ACTIVITY_NAME,"IN onSTOP()");
+  }
+
+  protected  void onDestroy(){
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME,"IN onDESTROY()");
+  }
 }
--- app/src/main/java/com/example/xiaoy/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/xiaoy/androidlabs/ProfileActivity.java
@@ -2,12 +2,22 @@ package com.example.xiaoy.androidlabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import static com.example.xiaoy.androidlabs.R.id.clickButton_profile;
 
 public class ProfileActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "ProfileActivity";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    ImageButton imageButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imageButton =(ImageButton)findViewById(clickButton_profile);
     }
 }
